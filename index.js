9fb380930c2a1435d79168de7e73da71e0bd9341
rellika <rellika@lwalacommunityalliance.org>
2022-11-02 20:52:14 +0300

    DEBUGGIN

--- index.js
+++ index.js
@@ -1,133 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const mongoSanitize = require('express-mongo-sanitize')
-const cors = require('cors')
-const connectDB = require('./config/db.js')
-require('dotenv').config()
-require('express-async-errors')
-const morgan = require('morgan')
-
-// Import middleware.
-const notFoundMiddleware = require('./middleware/not-found.js')
-const errorHandlerMiddleware = require('./middleware/error-handler.js')
-// const authenticateUser = require('./middleware/auth.js')
-
-// Import routes.
-const authRoutes = require('./routes/authRoutes.js')
-const infoRoutes = require('./routes/infoRoutes.js')
-
-//import user.js from util
-const { isRegistered } = require('./utils/user')
-const User = require('./models/user')
-//import menu.js
-const {
-	mainMenuRegistered,
-	mainMenuNotregistered,
-	registerMenu,
-	middleware,
-	gestationalDiabetesMenu,
-	settingMenu,
-	subscribeMenu,
-} = require('./menu')
-
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-if (process.env.NODE_ENV !== 'production') {
-	app.use(morgan('dev'))
-}
-
-app.use(mongoSanitize())
-app.use(cors())
-
-// Connect to MongoDB
-connectDB()
-
-app.use(function (req, res, next) {
-	res.setHeader('Content-Type', 'text/plain')
-	next()
-})
-
-//create a route for get request and respond with a message
-app.get('/', (req, res) => {
-	res.send('Welcome to the USSD API')
-})
-
-app.post('/', async (req, res) => {
+app.post('/', (req, res) => {
 	// Read the variables sent via POST from our API
 	const { sessionId, serviceCode, phoneNumber, text } = req.body
-	const userIsRegistered = await isRegistered(phoneNumber)
-	const cleanText = await middleware(text, sessionId, phoneNumber)
-	const user = await User.findOne({ phoneNumber })
 
 	let response = ''
-	if (cleanText === 'EXITMENU') {
-		response = 'END Thank you for using this service'
+
+	if (text == '') {
+		// This is the first request. Note how we start the response with CON
+		response = `CON What would you like to check
+        1. My account
+        2. My phone number`
+	} else if (text == '1') {
+		// Business logic for first level response
+		response = `CON Choose account information you want to view
+        1. Account number`
+	} else if (text == '2') {
+		// Business logic for first level response
+		// This is a terminal request. Note how we start the response with END
+		response = `END Your phone number is ${phoneNumber}`
+	} else if (text == '1*1') {
+		// This is a second level response where the user selected 1 in the first instance
+		const accountNumber = 'ACC100101'
+		// This is a terminal request. Note how we start the response with END
+		response = `END Your account number is ${accountNumber}`
 	}
 
-	//If user is registered and cleanText is empty
-	else if (userIsRegistered && cleanText == '') {
-		const language = user.language
-		//pass language to the mainMenuRegistered function
-		response = await mainMenuRegistered(user.name, language)
-	}
-	//If user is registered and cleanText is not empty
-	else if (userIsRegistered && cleanText != '') {
-		const language = user.language
-		const textArray = cleanText.split('*')
-		if (cleanText[0] == 1) {
-			response = await gestationalDiabetesMenu(
-				textArray,
-				language,
-				phoneNumber
-			)
-		} else if (cleanText[0] == 2) {
-			response = await settingMenu(textArray, language, user)
-		} else if (cleanText[0] == 3) {
-			response = await subscribeMenu(textArray, user, language)
-		} else {
-			response = 'END Invalid option'
-		}
-	}
-	//If user is not registered and cleanText is empty
-	else if (cleanText == '' && !userIsRegistered) {
-		response = `CON ${mainMenuNotregistered()}`
-	}
-
-	//If user is not registered and cleanText is not empty
-	else if (!userIsRegistered && cleanText !== '') {
-		if (cleanText[0] == '1') {
-			response = `CON ${await registerMenu(
-				cleanText.split('*'),
-				phoneNumber,
-				'en'
-			)}`
-		} else if (cleanText[0] == '2') {
-			response = `CON ${await registerMenu(
-				cleanText.split('*'),
-				phoneNumber,
-				'sw'
-			)}`
-		} else if (cleanText[0] == '3') {
-			response = `END Please call 999 112 or 911`
-		} else if (cleanText[0] == '4') {
-			response = `END Our plan was to connect the patient with a couple gyenacologist If we move forward to the next phase we will consult 4 gyenacologist for their services`
-		} else {
-			response = `END Invalid entry. Please try again`
-		}
-	}
+	// Send the response back to the API
 	res.set('Content-Type: text/plain')
 	res.send(response)
 })
-//Routers
-app.use('/api/auth', authRoutes)
-app.use('/api/info', infoRoutes)
-
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
-
-//Add port to listen to
-const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`Server started on port ${port}`))
